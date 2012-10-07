615e5cbbe6c13a8e19d7faca455f57044bce7e3e
camilo tapia <camilo.tapia@gmail.com>
2012-10-07 22:18:51 +0200

    moved client file into nunt

--- example/server.js
+++ example/server.js
@@ -16,46 +16,19 @@ var config = {
 // the event framework
 var nunt = require('../src/nunt/lib/nunt');
 
-function initServer(serverConfig)
-{
+// start the server
+var express = require('express');
+var app = express.createServer();
 
-    // start the server
-    var express = require('express');
-    var app = express.createServer();
+app.configure(function(){
+    app.use(express.static(__dirname + '/public'));
+});
 
-    app.configure(function(){
-        app.use(express.static(__dirname + '/public'));
-    });
+// connect the socket server
+app.listen(config.server.port);
 
-    // connect the socket server
-    app.listen(config.server.port);
+nunt.init({
+    server: app,
+    load: [__dirname + "/mvc"]
+});
 
-    return app;
-}
-
-
-function initNunt(server)
-{
-    
-    // log the start
-    console.log();
-    nunt.log("Starting eventeco server version " + config.version + "...");
-    
-    // read external modules in a batch
-    nunt.loadPaths([__dirname + "/mvc"]); 
-
-    //init the event system. in the browser this is done automatically, in node we trigger it manually
-    nunt.init(
-        {
-            server: {
-                server: server,
-                options: config.server
-            }
-        }
-    );
-
-}
-
-// begin!
-var app = initServer(config);
-initNunt(app);
